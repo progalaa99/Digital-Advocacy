dbbeb74dec06f8660c919ddd8457e3abdda83e04
Alaa Salah <sbwjhkm@gmail.com>
2023-04-29 16:34:20 +0300

    up

--- digital/resources/views/header.blade.php
+++ digital/resources/views/header.blade.php
@@ -16,6 +16,35 @@
         </style>
     </head>
 <body class="  antialiased ">
-    
+    <header class="flex items-center justify-between flex-wrap bg-gray-800 bg-darkalaa p-6">
+    <div class="  flex items-center flex-shrink-0 text-white mr-6">
+        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M27 0C12.1 0 0 12.1 0 27s12.1 27 27 27 27-12.1 27-27S41.9 0 27 0zm-7.8 32.7c-.9.9-1.5 2.2-1.5 3.6 0 2.8 2.2 5 5 5s5-2.2 5-5c0-1.4-.6-2.7-1.5-3.6l5.1-5.1c.8-.8.8-2 0-2.8l-2.8-2.8c-.8-.8-2-.8-2.8 0l-5.1 5.1z"/></svg>
+        <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">
+            Docs
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">
+            Examples
+          </a>
+          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white">
+            Blog
+          </a>
+        </div>
+        <div>
+          <a href="#"
+             class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white mt-4 lg:mt-0">Download</a>
+        </div>
+      </div>
+    </header>
+@yield('content')
+   
 </body>
 </html>
