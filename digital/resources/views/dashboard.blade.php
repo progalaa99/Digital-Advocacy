ad66899f83837ac7c1a4a4babecd22a4c303cb80
Alaa Salah <sbwjhkm@gmail.com>
2023-05-02 16:54:59 +0300

    up

--- digital/resources/views/dashboard.blade.php
+++ digital/resources/views/dashboard.blade.php
@@ -55,7 +55,7 @@
         </div>
     </div>
 
-
+    @extends('footer')
     {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
