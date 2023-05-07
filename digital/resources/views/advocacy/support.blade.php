002dc9fb0c6be3022d19a67e15ea9de4e73b676c
Alaa Salah <sbwjhkm@gmail.com>
2023-05-07 16:54:01 +0300

    up

--- digital/resources/views/advocacy/support.blade.php
+++ digital/resources/views/advocacy/support.blade.php
@@ -34,37 +34,41 @@
         </div> 
         </div>
       </div>
-
+{{-- <div class="flex w-1/2 mx-auto justify-center items-center">
+  <h1>view more</h1>
+</div> --}}
+<div class="flex w-1/2 mx-auto justify-center items-center">
+  <h1 class="text-6xl font-bold">Article</h1>
+</div>
 {{--   2 div     --}}
+
       <div class="flex w-1/2 mx-auto justify-center items-center">
+       
         <div class="w-1/3 bg-darkalaa p-4 m-2 rounded-md ">
             <img class="w-64 h-64 pt-10  transform -translate-x-1/9 -translate-y-1/11 fill-svgfill " fill="svgfill"
-            src="/photo/doctor.jpg" alt="head-idea-icon">
+            src="/photo/aoo.jpg" alt="head-idea-icon">
             <div class="pt-5">
-            <span class="text-1xl font-bold text-white">Name : Aya</span><br>
-            <span class="text-1xl font-bold text-white">Phone : 077******</span><br>
-            <span class="text-1xl font-bold text-white">Email : Aya@gmail.com</span><br>
-            <span class="text-1xl font-bold text-white">Adress : Baghdad</span><br>
+            <span class="text-1xl font-bold text-white">Steps to overcome psychological 
+              trauma</span><br>
+            
           </div>
         </div>
         <div class="w-1/3 bg-darkalaa p-4 m-2 rounded-md flex flex-col">
           <img class="w-64 h-64 pt-10  transform -translate-x-1/9 -translate-y-1/11 fill-svgfill " fill="svgfill"
-            src="/photo/doctor.jpg" alt="head-idea-icon">
+            src="/photo/aoo.jpg" alt="head-idea-icon">
             <div class="pt-5">
-            <span class="text-1xl font-bold text-white">Name : Aya</span><br>
-            <span class="text-1xl font-bold text-white">Phone : 077******</span><br>
-            <span class="text-1xl font-bold text-white">Email : Aya@gmail.com</span><br>
-            <span class="text-1xl font-bold text-white">Adress : Baghdad</span><br>
+            <span class="text-1xl font-bold text-white">Steps to overcome psychological 
+              trauma</span><br>
+            
           </div>
         </div>
         <div class="w-1/3 bg-darkalaa p-4 m-1 rounded-md  ">
           <img class="w-64 h-64 pt-10  transform -translate-x-1/9 -translate-y-1/11 fill-svgfill " fill="svgfill"
-          src="/photo/doctor.jpg" alt="head-idea-icon">
+          src="/photo/aoo.jpg" alt="head-idea-icon">
           <div class="pt-5">
-          <span class="text-1xl font-bold text-white">Name : Aya</span><br>
-          <span class="text-1xl font-bold text-white">Phone : 077******</span><br>
-          <span class="text-1xl font-bold text-white">Email : Aya@gmail.com</span><br>
-          <span class="text-1xl font-bold text-white">Adress : Baghdad</span><br>
+          <span class="text-1xl font-bold text-white">Steps to overcome psychological 
+            trauma</span><br>
+          
         </div> 
         </div>
       </div>
