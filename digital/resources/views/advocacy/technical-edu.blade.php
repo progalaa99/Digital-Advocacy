37da6ed86a627bcb507b683375b96a46353a29b1
Alaa Salah <sbwjhkm@gmail.com>
2023-05-08 15:48:50 +0300

    Update technical-edu.blade.php

--- digital/resources/views/advocacy/technical-edu.blade.php
+++ digital/resources/views/advocacy/technical-edu.blade.php
@@ -1,8 +1,20 @@
 <x-app-layout>
-    <div class="flex items-center flex-row items-center   absolute  top-3/2 left-60 pt-10  ">
+    <div class="flex items-center flex-row items-center   absolute  top-3/2 left-60 pt-10 border  w-auto p-10  ">
        
-      
+      <div class="flex grid grid-flow-col auto-cols-max">
+        <div class="w-1/2 bg-gray-300">
+          <div class="w-10 bg-gray-300">sbj</div>
+          <div class="w-1/2 bg-gray-500">ask</div>
+        </div>
+
+        <div class="w-1/2 bg-gray-500">
+        <h1 class="text-3xl text-white">sbject</h1>
+        
+        </div>
+       
       </div>
+      
+    </div>
       
       
     @extends('footer')
