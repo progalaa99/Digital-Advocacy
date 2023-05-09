5eaf6a1db82cafa7444447c9970c1c65c410332a
Alaa Salah <sbwjhkm@gmail.com>
2023-05-09 17:38:16 +0300

    Update create.blade.php

--- digital/resources/views/blogedu/create.blade.php
+++ digital/resources/views/blogedu/create.blade.php
@@ -1,30 +1,30 @@
 <x-app-layout>
-    <div class="bg-darkalaa p-4 w-3/2  border flex items-center">
-        <form class="flex flex-col items-center left-1/2 top-1/2 border w-1/2 " action="" method="POST" enctype="multipart/form-data">
+    <div class="bg-darkalaa p-4 w-3/2   flex justify-center items-center h-screen">
+        <form class="flex flex-col items-center left-1/2 top-1/2  w-1/2 pt-10 " action="" method="POST" enctype="multipart/form-data">
           @csrf
-            <label class="block p-2">
-              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
+            <label class="block  text-gray-700 font-bold mb-2   w-full h-128">
+              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block   text-white text-2xl">
                 Title
               </span>
-              <input type="text" name="name" class="mt-1 px-10 py-3 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"  />
-            </label>
+              <input type="text" name="name" class="mt-1 px-10 py-3 h-16 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"  />
+            </label> <br>
             {{-- <label class="block p-2">
               <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
                Body
               </span>
               <input type="text" name="nameblackmailer" class="mt-1 px-10 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"  />
             </label> --}}
-            <label class="block text-gray-700 font-bold mb-2 border  w-full h-128">
-              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
+            <label class="block text-gray-700 font-bold mb-2   w-full h-128">
+              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-white text-2xl text-white">
                 body
               </span>
               <textarea type="text" name="Blackmailerinfo" class="border-gray-400 border rounded py-2 px-3 w-full h-64"  ></textarea>
 
-            </label>
+            </label><br>
             <label class="block p-2 flex-col items-center "> 
               
-                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
-                    Choose profile photo</span> 
+                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-white text-2xl text-white text-center">
+                    Choose Blog Cover</span> 
                <input type="file" name="file" class="  block w-full text-sm text-slate-500 pl-20
                 file:mr-4 file:py-2 file:px-3
                 file:rounded-full file:border-0
@@ -32,7 +32,7 @@
                 file:bg-violet-50 file:text-violet-700
                 hover:file:bg-violet-100
               "/> 
-            </label>
+            </label><br>
            
             
             <div class="bg-buttonalaa border-none   rounded ">
