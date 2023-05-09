87d6d678b2ddb3dd67ecf9a8f3743f1cf43c9588
Alaa Salah <sbwjhkm@gmail.com>
2023-05-09 16:09:31 +0300

    up

--- digital/resources/views/advocacy/technical-edu.blade.php
+++ digital/resources/views/advocacy/technical-edu.blade.php
@@ -1,22 +1,60 @@
 <x-app-layout>
-    <div class="flex items-center flex-row items-center   absolute  top-3/2 left-60 pt-10 border  w-auto p-10  ">
-       
-      <div class="flex grid grid-flow-col auto-cols-max">
-        <div class="w-1/2 bg-gray-300">
-          <div class="w-10 bg-gray-300">sbj</div>
-          <div class="w-1/2 bg-gray-500">ask</div>
-        </div>
+  <div class="flex top-1/2 left-1/2 text-white">
+    
+    <div class="w-full flex justify-center flex-col  text-white">
+      <div class="flex w-1/2 mx-auto justify-center items-center">
+        <div class="w-1/3 bg-darkalaa p-4 m-2 rounded-md border-b-2">
+            <img class="w-64 h-64 pt-10  transform -translate-x-1/9 -translate-y-1/11 fill-svgfill " fill="svgfill"
+            src="/photo/doctor.jpg" alt="head-idea-icon">
+            <div class="pt-5">
+            <span class="text-1xl font-bold text-white text-center">Titel about artical</span><br>
+            </div>
 
-        <div class="w-1/2 bg-gray-500">
-        <h1 class="text-3xl text-white">sbject</h1>
-        
+        <div >
+          <a href="" class="bg-buttonalaa w-6 p-1 mr-2  w-20 h-30 border-none   rounded">View</a>
         </div>
-       
+        </div>
+        
+        <div class="w-1/3 bg-darkalaa p-4 m-2 rounded-md flex flex-col border-b-2">
+          <img class="w-64 h-64 pt-10  transform -translate-x-1/9 -translate-y-1/11 fill-svgfill " fill="svgfill"
+            src="/photo/doctor.jpg" alt="head-idea-icon">
+            <div class="pt-5">
+            <span class="text-1xl font-bold text-white">Name : Aya</span><br>
+            
+          </div>
+          <div >
+            <a href="" class="bg-buttonalaa w-6 p-1 mr-2  w-20 h-30 border-none   rounded">View</a>
+          </div>
+        </div>
+        <div class="w-1/3 bg-darkalaa p-4 m-1 rounded-md border-b-2 ">
+          <img class="w-64 h-64 pt-10  transform -translate-x-1/9 -translate-y-1/11 fill-svgfill " fill="svgfill"
+          src="/photo/doctor.jpg" alt="head-idea-icon">
+          <div class="pt-5">
+          <span class="text-1xl font-bold text-white">Name : Aya</span><br>
+          
+        </div> 
+        <div >
+          <a href="" class="bg-buttonalaa w-6 p-1 mr-2  w-20 h-30 border-none   rounded">View</a>
+        </div>
+        </div>
       </div>
+
+
       
-    </div>
-      
-      
+{{-- <div class="flex w-1/2 mx-auto justify-center items-center">
+  <h1>view more</h1>
+</div> --}}
+
+{{--   2 div     --}}
+
+     
+       
+
+    
+    
+
+   
+    
     @extends('footer')
    
 </x-app-layout>
