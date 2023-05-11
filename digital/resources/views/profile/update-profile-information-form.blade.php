28d9bb5a4e41ec51feadb88bdf0a9f4bb384565c
Alaa Salah <sbwjhkm@gmail.com>
2023-05-11 19:02:31 +0300

    up

--- digital/resources/views/profile/update-profile-information-form.blade.php
+++ digital/resources/views/profile/update-profile-information-form.blade.php
@@ -3,7 +3,7 @@
         {{ __('Profile Information') }}
     </x-slot>
 
-    <x-slot name="description">
+    <x-slot name="description" >
         {{ __('Update your account\'s profile information and email address.') }}
     </x-slot>
 
