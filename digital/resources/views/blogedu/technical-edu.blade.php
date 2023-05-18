6952ab52cdd24d24e18d6f26643b0bfc22a6f4f6
Alaa Salah <sbwjhkm@gmail.com>
2023-05-18 19:17:50 +0300

    up

--- digital/resources/views/blogedu/technical-edu.blade.php
+++ digital/resources/views/blogedu/technical-edu.blade.php
@@ -1,14 +1,19 @@
 <x-app-layout>
   <div class="flex top-1/2 left-1/2 text-white">
    
-        
+     
     
     <div class="w-full flex justify-center flex-col  text-white">
+      @if (auth()->user()->roleId === 1)
+    <div >
+      <a href="{{route('blogedu.create')}}" class="bg-buttonalaa w-6 p-1 mr-2  w-20 h-30 border-none   rounded">Add Blog</a>
+    </div> 
+     @endif  
      
       <div class="flex w-1/2 mx-auto justify-center items-center">
         
         @foreach ($blogedus as $blogedu)
-       
+      
         <div class="w-1/3 bg-darkalaa p-4 m-2 rounded-md border-b-2 ">
             <img class="w-64 h-64 pt-10  transform -translate-x-1/9 -translate-y-1/11 fill-svgfill " fill="svgfill"
             src="{{$blogedu->cover}}" alt="head-idea-icon">
@@ -19,7 +24,13 @@
         <div >
           <a href="{{route('blogedu.show',['id'=>$blogedu->id])}}" class="bg-buttonalaa w-6 p-1 mr-2  w-20 h-30 border-none   rounded">View</a>
         </div>
-        
+        @if (auth()->user()->roleId === 1)
+    <div >
+      <br>
+      <a href="{{route('blogedu.create')}}" class="bg-buttonalaa w-6 p-1 mr-2  w-20 h-30 border-none   rounded">Edit</a>
+      <a href="{{route('blogedu.create')}}" class="bg-red-900 w-6 p-1 mr-2  w-20 h-30 border-none   rounded">Delete</a>
+    </div> 
+     @endif 
         </div>
        
         @endforeach
--- digital/resources/views/dashboard.blade.php
+++ digital/resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="flex antialiased ">
 
         <div class="bg-darkalaa p-48 mr-48 ">
-            <h1 class="text-8xl text-white font-bold ">Welcome</h1>
+           
             <h2 class="text-2xl text-white">We are here to support you and help you solve the problem of
                 electronic extortion, <br> with the availability of many services that
                 help you to use the Internet safely,<br> do not hesitate to browse
