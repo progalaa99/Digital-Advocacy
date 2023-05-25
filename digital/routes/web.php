2ac2a7415ae88866bfc1e5956b1b3c5072e06364
Alaa Salah <sbwjhkm@gmail.com>
2023-05-25 20:58:13 +0300

    Update web.php

--- digital/routes/web.php
+++ digital/routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware(['auth'])->name('verification.notice');
 
 Route::middleware([
     'auth:sanctum',
